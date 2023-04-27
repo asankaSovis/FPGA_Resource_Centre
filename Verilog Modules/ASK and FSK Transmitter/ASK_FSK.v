ac4b4b58923337613de359ce72f47c4e52ca9b71
Asanka Akash Sovis <46389631+asankaSovis@users.noreply.github.com>
2023-04-27 11:59:53 +0530

    Added comments

--- Verilog Modules/ASK and FSK Transmitter/ASK_FSK.v
+++ Verilog Modules/ASK and FSK Transmitter/ASK_FSK.v
@@ -1,3 +1,21 @@
+/*
+ * ------------------------------------------------------------------
+ *                  ASK-FSK TRANSMITTER v1.0
+ * 
+ * This module recieves a bitstream and transmit it through both ASK
+ * and FSK combined transmission method. Under ASK, two amplitude levels
+ * are used while under FSK, two frequecies are used. Thus, combining
+ * both allows two bits to be sent simultaneously.
+ * 
+ * Written Date: 02/03/2023
+ * Filename: ASK_FSK.v
+ * Last Edited: 27/04/2023
+ * Author: Asanka Sovis
+ * 
+ *      (C) 2023, Asanka Sovis
+ * ------------------------------------------------------------------
+ */
+
 module ASK_FSK (int_clk, clk, bitstream, out_red, out_green);
 input int_clk, clk, bitstream;
 output out_red, out_green;
--- Verilog Modules/Hamming 74 Modulator-Demodulator/hamming74_enc.v
+++ Verilog Modules/Hamming 74 Modulator-Demodulator/hamming74_enc.v
@@ -1,3 +1,25 @@
+/*
+ * ------------------------------------------------------------------
+ *                  HAMMING 74 ENCODER v1.0
+ * 
+ * Hamming code (Hamming(7,4)) is a type of error-correcting code used
+ * to detect and correct errors that may occur when transmitting digital
+ * data. The code uses four data bits and adds three parity bits to form
+ * a seven-bit code word. These parity bits are calculated based on the
+ * data bits using a specific algorithm. The code can detect and correct
+ * single-bit errors, making it useful in applications where data
+ * integrity is critical, such as in computer memory or communication
+ * systems. The Hamming code was invented by Richard Hamming in the
+ * 1950s and is still widely used today.
+ * 
+ * Written Date: 27/042023
+ * Filename: hamming74_enc.v
+ * Last Edited: 27/04/2023
+ * Author: Asanka Sovis
+ * 
+ *      (C) 2023, Asanka Sovis
+ * ------------------------------------------------------------------
+ */
 
 module hamming74_enc(
     input  [8:0] i_data,
